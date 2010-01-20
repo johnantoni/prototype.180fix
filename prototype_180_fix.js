c8d1a8ff8fe43da47448d38fe8bc8e00e0b7b2c4
John <john@theworkinggroup.ca>
2010-01-20 23:19:23 +0000

    init

---
+++ prototype_180_fix.js
@@ -0,0 +1,6 @@
+﻿//parent.subclass undefined fix
+Class.create = Class.create.wrap(function(proceed, parent) {
+  if (Object.isFunction(parent))
+    parent.subclasses = parent.subclasses || [];
+  return proceed.apply(Class, $A(arguments).slice(1));
+})
